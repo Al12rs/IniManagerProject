2a46d74238c7cb8d6f4781090f5196c72da39154
hjk22 <barlotz22@gmail.com>
2019-08-06 17:53:50 +0200

    Added dynamic tabs and open existing document window

--- INIManagerProject/MainWindow.xaml.cs
+++ INIManagerProject/MainWindow.xaml.cs
@@ -43,13 +43,99 @@ namespace INIManagerProject
                 this.AddTabItem();
 
                 // bind tab control
-                tabDynamic.DataContext = _tabItems;
+                document.DataContext = _tabItems;
 
-                tabDynamic.SelectedIndex = 0;
+                document.SelectedIndex = 0;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = document.FindResource("TabHeader") as DataTemplate;
+
+            // add controls to tab item, this case I added just a textbox
+            TextBox txt = new TextBox();
+            txt.Name = "txt";
+            DocumentView dv = new DocumentView();
+            tab.Content = dv;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+            return tab;
+        }
+
+        private void document_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = document.SelectedItem as TabItem;
+
+            if (tab != null && tab.Header != null)
+            {
+                if (tab.Header.Equals("+"))
+                {
+                    // clear tab control binding
+                    document.DataContext = null;
+
+                    // add new tab
+                    TabItem newTab = this.AddTabItem();
+
+                    // bind tab control
+                    document.DataContext = _tabItems;
+
+                    // select newly added tab item
+                    document.SelectedItem = newTab;
+                }
+                else
+                {
+                    // your code here...
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = document.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (_tabItems.Count < 3)
+                {
+                    MessageBox.Show("Cannot remove last tab.");
+                }
+                else if (MessageBox.Show(string.Format("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
+                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = document.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    document.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    document.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    document.SelectedItem = selectedTab;
+                }
             }
         }
 
@@ -58,7 +144,9 @@ namespace INIManagerProject
 
             // Placeholder: hardcoded to open skyrim.ini document.
             ((App)Application.Current).IniApplication.DocumentManager.CreateAndLoadDocumentFromName("Skyrim");
-
+            //generate new window
+            var openDocumentWindow = new OpenExistingDocumentWindow();
+            openDocumentWindow.Show();
 
         }
 
---
+++ INIManagerProject/OpenExistingDocumentWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INIManagerProject
+{
+    /// <summary>
+    /// Interaction logic for OpenExistingDocumentWindow.xaml
+    /// </summary>
+    public partial class OpenExistingDocumentWindow : Window
+    {
+        public OpenExistingDocumentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            //load selected document
+            //((App)Application.Current).IniApplication.DocumentManager.CreateAndLoadDocumentFromName("Skyrim");
+            this.Close();
+        }
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
