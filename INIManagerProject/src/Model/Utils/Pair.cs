8211fff88d6cc3c1f60325f41d2a33b2eda14cb8
Al <gabriel.cortesi@outlook.com>
2019-08-26 19:03:05 +0200

    Added new utility class Pair. This one is a mutable class as opposed to KeyValuePair.

---
+++ INIManagerProject/src/Model/Utils/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INIManagerProject.Model.Utils
+{
+    class Pair<KeyT, ValueT>
+    {
+        public KeyT Key { get; set; }
+        public ValueT Value { get; set; }
+
+        public Pair() { }
+
+        public Pair(KeyT key, ValueT value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
