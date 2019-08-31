93151b5a79621c2b64f4b6407e0a8bb23b681cda
Al <gabriel.cortesi@outlook.com>
2019-08-31 16:49:40 +0200

    Moved the inputdialog to View package.

--- INIManagerProject/src/View/InputDialogue.xaml.cs
+++ INIManagerProject/src/View/InputDialogue.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace INIManagerProject
+namespace INIManagerProject.View
 {
     /// <summary>
     /// Logica di interazione per InputDialogue.xaml
